342ba2813d0f31583f44f09803f4d4d739d7612d
kimaita <johnnieqaym@gmail.com>
2024-08-20 02:26:13 +0300

    added task #1

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+""""""
+
+import asyncio
+from typing import List
+
+wait_random = __import__("0-basic_async_syntax").wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """Calls wait_random n times, returning a list of random floats of length n
+
+    Args:
+        n(int)
+        max_delay(int)
+
+    Returns:
+        list of random delays
+    """
+
+    r = await asyncio.gather(*(wait_random(max_delay) for i in range(n)))
+    return r
