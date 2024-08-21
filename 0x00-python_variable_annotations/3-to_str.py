a5736ffce8bdfe05080a0b269de7fb1924f4c3c3
kimaita <johnnieqaym@gmail.com>
2024-08-21 05:14:34 +0300

    added task #3

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Contains a function for type conversion"""
+
+
+def to_str(n: float) -> str:
+    """Returns the string representation of n
+
+    Args:
+        n(float)
+
+    Returns:
+        str
+    """
+    return str(n)
