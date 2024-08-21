6e9d70b94843251d9a86438d362f08b523ba14f5
kimaita <johnnieqaym@gmail.com>
2024-08-21 11:53:56 +0300

    changed return type annotation
    use typing.Generator in placeof typing.AsyncGenerator

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -3,10 +3,10 @@
 
 import asyncio
 from random import uniform
-from typing import AsyncGenerator
+from typing import Generator
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator() -> Generator[float, None, None]:
     """Async generator that yields a random float between 0 and 10
     every second.
     """
