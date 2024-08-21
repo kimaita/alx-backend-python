c7e1c70466c846ebd0b43bb8191c865088163ae3
kimaita <johnnieqaym@gmail.com>
2024-08-21 04:35:45 +0300

    added docstrings

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-""""""
+"""Contains an async comprehension"""
 
 from typing import List
 
@@ -7,5 +7,9 @@ async_generator = __import__("0-async_generator").async_generator
 
 
 async def async_comprehension() -> List[float]:
-    """"""
+    """Async comprehension for an async generator
+
+    Returns:
+        list of random floats
+    """
     return [i async for i in async_generator()]
