98ea3b52ffa0a93f5e5622d142c385963d530b39
kimaita <johnnieqaym@gmail.com>
2024-08-21 05:09:11 +0300

    added task #2

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Contains a rounding function"""
+
+import math
+
+
+def floor(n: float) -> int:
+    """Returns the floor of n, i.e. largest integer less than or equal to n."""
+    return math.floor(n)
