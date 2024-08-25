6a0d6d1b5d577ab0a234d4eb64ad30b31d7271a8
kimaita <johnnieqaym@gmail.com>
2024-08-25 10:27:29 +0300

    fixed problems
    Code edited to comply with type annotations and checked with mypy.

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -1,14 +1,24 @@
 #!/usr/bin/env python3
 """This code is validated using mypy and corrected accordingly"""
 
+from typing import List, Tuple
 
-def zoom_array(lst: Tuple, factor: int = 2) -> Tuple:
-    zoomed_in: Tuple = [item for item in lst for i in range(factor)]
+
+def zoom_array(lst: Tuple, factor: int = 2) -> List:
+    """Repeats each element of lst factor times
+
+    Args:
+        lst: a tuple of ints
+        factor
+
+    Returns:
+        A list with each element of lst repeated factor times
+    """
+    zoomed_in: List = [item for item in lst for i in range(factor)]
     return zoomed_in
 
 
-array = [12, 72, 91]
+array = (12, 72, 91)
 
 zoom_2x = zoom_array(array)
-
-zoom_3x = zoom_array(array, 3.0)
+zoom_3x = zoom_array(array, 3)
